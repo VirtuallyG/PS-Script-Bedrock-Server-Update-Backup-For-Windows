723bff77cae84582b19e42e37fc2f0ab18c8d220
VirtuallyG <47230170+VirtuallyG@users.noreply.github.com>
2025-07-07 16:06:55 -0400

    Update StopServerScript.ps1

--- StopServerScript.ps1
+++ StopServerScript.ps1
@@ -1,2 +1,21 @@
 # Right Click the file and choose [Run with PowerShell] to stop a server that was started in the background by the Task Scheduler
-Stop-Process -name "bedrock_server"
+#CHANGE THE VARIABLE BELOW TO YOUR SERVER EXECUTABLE NAME IF DIFFERENT FROM DEFAULT, AND SAVE THE SCRIPT
+$bedServerExe = "bedrock_server_competitive.exe"
+
+# Check for the process and stop it if it exists
+$processName = $bedServerExe -replace '.exe',''
+
+Write-Host "Checking for server process: $processName"
+Start-Sleep -Seconds 2
+
+$process = Get-Process -Name $processName -ErrorAction SilentlyContinue
+
+if ($process) {
+    Write-Host "Process found. STOPPING SERVICE..."
+    Stop-Process -Name $processName
+    Write-Host "Process stopped"
+    Start-Sleep -Seconds 10
+} else {
+    Write-Host "Process was not found. Nothing to stop."
+    Start-Sleep -Seconds 10
+}
